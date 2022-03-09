a8b0ae62819640777c62ea08259548a7dc14b3e3
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2022-03-09 17:19:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,3 +27,25 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded() {
+    console.log('posenet is initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist = " + scoreRightWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log('leftWristX = ' + leftWristX + 'leftwristy = ' + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('rightWristX = ' + rightWristX + 'rightwristy = ' + rightWristY);
+    }
+}
